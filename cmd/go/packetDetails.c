ae32ae63d50d9477ac90ac0a7e6bb6c7fbda1ad9
David-VTUK <david.holder@gmail.com>
2024-04-14 11:54:33 +0000

    add l4 info

--- cmd/go/packetDetails.c
+++ cmd/go/packetDetails.c
@@ -4,9 +4,11 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
+#include <netinet/in.h> 
 #include <linux/if_ether.h>
 #include <linux/ip.h>
 #include <linux/udp.h>
+#include <linux/tcp.h>
 #include <string.h>
 
 struct {
@@ -16,15 +18,17 @@ struct {
 
 struct packetDetails
 {
-    unsigned char l2_src_addr[6];
-    unsigned char l2_dst_addr[6];
+    char l2_src_addr[6];
+    char l2_dst_addr[6];
     unsigned int l3_src_addr;
     unsigned int l3_dst_addr;
     unsigned int l3_protocol;
     unsigned int l3_length;
     unsigned int l3_ttl;
     unsigned int l3_version;
-
+    char l4_protocol[4];
+    unsigned int l4_src_port;
+    unsigned int l4_dst_port;
 };
 
 SEC("xdp")
@@ -34,6 +38,8 @@ int packet_details(struct xdp_md *ctx) {
 
     struct ethhdr *eth = data;
     struct iphdr *ip;
+    struct udphdr *udp;
+    struct tcphdr *tcp;
 
     struct packetDetails *packet;
 
@@ -48,15 +54,19 @@ int packet_details(struct xdp_md *ctx) {
     if ((void *)(ip + 1) > data_end)
         return XDP_PASS;
 
-    // Reserve space in the ring buffer
-    packet = bpf_ringbuf_reserve(&rb,sizeof(*packet), 0);
-  
-    if (!packet) {
-        // Ideally, we'd handle not being able to
-        // reserve space, for testing purposes we'll
-        // simply allow it
-        return XDP_PASS;
+    // Extract TCP or UDP header
+    if (ip->protocol == IPPROTO_TCP) {
+        tcp = (struct tcphdr *)(ip + 1);
+        strcpy(packet->l4_protocol, "TCP");
+        packet->l4_src_port = tcp->source;
+        packet->l4_dst_port = tcp->dest;
+    } else if (ip->protocol == IPPROTO_UDP) {
+        udp = (struct udphdr *)(ip + 1);
+        strcpy(packet->l4_protocol, "UDP");
+        packet->l4_src_port = udp->source;
+        packet->l4_dst_port = udp->dest;
     }
+
 
     memcpy(packet->l2_dst_addr, eth->h_dest, 6);
     memcpy(packet->l2_src_addr, eth->h_source,6);
@@ -67,10 +77,19 @@ int packet_details(struct xdp_md *ctx) {
     packet->l3_ttl = ip->ttl;
     packet->l3_version = ip->version;
 
+    // Reserve space in the ring buffer
+    packet = bpf_ringbuf_reserve(&rb,sizeof(*packet), 0);
+  
+    if (!packet) {
+        // Ideally, we'd handle not being able to
+        // reserve space, for testing purposes we'll
+        // simply allow it
+        return XDP_PASS;
+    }
+
     bpf_ringbuf_submit(packet, 0);
 
     return XDP_PASS;
-
 }
 
 
--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.22.1
 require github.com/cilium/ebpf v0.14.0
 
 require (
-	golang.org/x/exp v0.0.0-20230224173230-c95f2b4c22f2 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/exp v0.0.0-20240409090435-93d18d7e34b8 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 )
